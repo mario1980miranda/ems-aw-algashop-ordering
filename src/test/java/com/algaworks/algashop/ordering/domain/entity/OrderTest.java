ccb9939ecae53730789cd22e7d29a071f51e1660
Mario Miranda <mario.luiz.miranda@gmail.com>
2026-09-11 19:18:15 -0400

    Order and OrderItem

---
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderTest.java
@@ -0,0 +1,87 @@
+package com.algaworks.algashop.ordering.domain.entity;
+
+import com.algaworks.algashop.ordering.domain.valueobject.Money;
+import com.algaworks.algashop.ordering.domain.valueobject.ProductName;
+import com.algaworks.algashop.ordering.domain.valueobject.Quantity;
+import com.algaworks.algashop.ordering.domain.valueobject.id.CustomerId;
+import com.algaworks.algashop.ordering.domain.valueobject.id.ProductId;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+class OrderTest {
+
+    @Test
+    void shouldGenerate() {
+
+        final var order = Order.draft(new CustomerId());
+
+        assertThat(order).isNotNull();
+    }
+
+    @Test
+    void shouldAddItem() {
+
+        final var order = Order.draft(new CustomerId());
+        final var productId = new ProductId();
+
+        order.addItem(
+                productId,
+                new ProductName("Mouse pad"),
+                new Money("100"),
+                new Quantity(1)
+        );
+
+        assertThat(order.items()).isNotEmpty();
+        assertThat(order.items()).hasSize(1);
+        final var orderItem = order.items().iterator().next();
+        assertWith(orderItem,
+                i -> assertThat(i.id()).isNotNull(),
+                i -> assertThat(i.productName()).isEqualTo(new ProductName("Mouse pad")),
+                i -> assertThat(i.productName()).isEqualTo(new ProductName("Mouse pad")),
+                i -> assertThat(i.productId()).isEqualTo(productId),
+                i -> assertThat(i.price()).isEqualTo(new Money("100")),
+                i -> assertThat(i.quantity()).isEqualTo(new Quantity(1))
+        );
+    }
+
+    @Test
+    void shouldGenerateExceptionWhenTryToChangeItemSet() {
+
+        final var order = Order.draft(new CustomerId());
+        final var productId = new ProductId();
+        order.addItem(
+                productId,
+                new ProductName("Mouse pad"),
+                new Money("100"),
+                new Quantity(1)
+        );
+
+        Set<OrderItem> items = order.items();
+        assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(items::clear);
+    }
+
+    @Test
+    void shouldCalculateTotals() {
+
+        final var order = Order.draft(new CustomerId());
+        final var productId = new ProductId();
+        order.addItem(
+                productId,
+                new ProductName("Mouse pad"),
+                new Money("100"),
+                new Quantity(2)
+        );
+        order.addItem(
+                productId,
+                new ProductName("RAM memory"),
+                new Money("50"),
+                new Quantity(1)
+        );
+
+        assertThat(order.totalAmount()).isEqualTo(new Money("250"));
+        assertThat(order.totalItems()).isEqualTo(new Quantity(3));
+    }
+}
