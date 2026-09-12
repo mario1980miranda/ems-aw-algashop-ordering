bc7eb4f674c303fa038e752336acb0cb27c41443
Mario Miranda <mario.luiz.miranda@gmail.com>
2026-09-12 12:34:56 -0400

    Order status control

---
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderStatusTest.java
@@ -0,0 +1,20 @@
+package com.algaworks.algashop.ordering.domain.entity;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderStatusTest {
+
+    @Test
+    void canChangeTo() {
+        Assertions.assertThat(OrderStatus.DRAFT.canChangeTo(OrderStatus.PLACED)).isTrue();
+        Assertions.assertThat(OrderStatus.DRAFT.canChangeTo(OrderStatus.CANCELED)).isTrue();
+        Assertions.assertThat(OrderStatus.PAID.canChangeTo(OrderStatus.DRAFT)).isFalse();
+    }
+
+    @Test
+    void canNotChangeTo() {
+        Assertions.assertThat(OrderStatus.PLACED.canNotChangeTo(OrderStatus.DRAFT)).isTrue();
+        Assertions.assertThat(OrderStatus.READY.canNotChangeTo(OrderStatus.PAID)).isTrue();
+    }
+}
